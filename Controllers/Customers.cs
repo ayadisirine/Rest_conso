e4d565d36b719540bfc8a66376321eb314aee8d3
ayadisirine <sirine.ayadi93@gmail.com>
2021-04-06 11:28:00 -0600

    adding customer

---
+++ Controllers/Customers.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Rocket_Elevators_Rest_API.Models;
+using Microsoft.EntityFrameworkCore;
+using Rocket_Elevators_Rest_API.Data;
+using Pomelo.EntityFrameworkCore.MySql;
+
+namespace Rocket_Elevators_Rest_API.Models.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly  rocketelevators_developmentContext _context;
+
+        public CustomersController( rocketelevators_developmentContext context)
+        {
+            _context = context;
+
+        }
+
+        // To see all customer                                         
+        // GET: api/customer/all           
+        [HttpGet("all")]
+        public IEnumerable<Customers> Getcustomers()
+        {
+            IQueryable<Customers> Customers =
+                from l in _context.Customers
+                select l;
+            return Customers.ToList();
+        }
+
+        [HttpGet("{email}")]
+        // User is free to check different status : in our case just make intervention 
+        public IEnumerable<Customers> checkUser(string email)
+        {
+            //Prepare the request 
+            IQueryable<Customers> customers = from l in _context.Customers
+            //define condition status should be equal to given values 
+                                             where l.TechnicalAuthorityEmail == email
+                                             select l;
+            //show results 
+            return customers.ToList();
+
+        }
+
+
+
+    }
+}
